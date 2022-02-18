5eb744cb93dbd6da701f05124c491e1c864bbd70
oreskovic.dino <oreskovic.dino@gmail.com>
2022-02-18 22:04:12 +0100

    Abstract Demo Code Commit

---
+++ AbstractDemo/ConsoleDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleDemo
+{
+    using DemoLibrary;
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<DataAccess> database = new List<DataAccess>()
+            {
+                new SqlDataAccess(),
+                new SqliteDataAccess(),
+            };
+
+            foreach (var db in database)
+            {
+                //db.LoadConnectionString("demo");
+                //db.LoadData("select * from table");
+                //db.SaveData("insert into table");
+
+                Console.WriteLine(db.LoadConnectionString("Demo"));
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AbstractDemo/DemoLibrary/SqlDataAccess.cs
@@ -0,0 +1,22 @@
+﻿namespace DemoLibrary
+{
+    public class SqlDataAccess : DataAccess
+    {
+        public override string LoadConnectionString(string name)
+        {
+            string output = base.LoadConnectionString(name);
+            output += " (from SqlData)";
+            return output;
+        }
+
+        public override void LoadData(string sql)
+        {
+            System.Console.WriteLine("Loading Microsoft SQL Data");
+        }
+
+        public override void SaveData(string sql)
+        {
+            System.Console.WriteLine("Saving data to Microsoft SQL Server");
+        }
+    }
+}
